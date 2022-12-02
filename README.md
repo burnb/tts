441f41840354c9ceac22804a540bcabdbff7b9b1
Aleksandr Petrov <burnb83@gmail.com>
2022-12-02 13:35:43 +0700

    fixed README.md

--- README.md
+++ README.md
@@ -10,14 +10,13 @@ REGION={Azure Region}
 ## Usage
 
 ```
-usage: tts [-i INPUT_FILE] [-s FILE_SEPARATOR] [-l ] [-v VOICE]
+Usage: tts [OPTIONS]
+Generate batch audio files with speech synthesis service(Provided by Azure Cognitive Services)
 
-Generate batch audio files by speech synthesis service(Provided by Azure Cognitive Services) in your terminal!
-
-options:
+Options:
   -h, --help            Show this help message and exit
   -i INPUT_FILE         Input file path (default "text.txt")
-  -l FILE_SEPARATOR     Locale (default "th-TH")
-  -s LOCALE             Columns separator in the input file (default "/")
+  -s FILE_SEPARATOR     Columns separator in the input file (default "/")
+  -l LOCALE             Locale (default "th-TH")
   -v VOICE              Voice (default "th-TH-AcharaNeural")
 ```
--- internal/configs/app.go
+++ internal/configs/app.go
@@ -2,11 +2,15 @@ package configs
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/kelseyhightower/envconfig"
 )
 
 const (
+	Name = "tts"
+
 	defaultFilePath  = "text.txt"
 	defaultSeparator = "/"
 	defaultLocale    = "th-TH"
@@ -20,19 +24,37 @@ type App struct {
 	Separator    string
 	LocaleString string
 	Voice        string
+	flagSet      *flag.FlagSet
 }
 
 // Prepare variables to static configuration
 func (c *App) Prepare() (err error) {
+	c.flagSet = flag.NewFlagSet(Name, flag.ExitOnError)
+	c.flagSet.Usage = c.printUsage
+
 	if err := envconfig.Process("", c); err != nil {
 		return err
 	}
 
-	flag.StringVar(&c.FilePath, "i", defaultFilePath, "input file path")
-	flag.StringVar(&c.Separator, "s", defaultSeparator, "columns separator in the input file")
-	flag.StringVar(&c.LocaleString, "l", defaultLocale, "locale")
-	flag.StringVar(&c.Voice, "v", defaultVoice, "voice")
-	flag.Parse()
+	help := c.flagSet.Bool("h", false, "Show this help message and exit")
+	c.flagSet.StringVar(&c.FilePath, "i", defaultFilePath, "input file path")
+	c.flagSet.StringVar(&c.Separator, "s", defaultSeparator, "columns separator in the input file")
+	c.flagSet.StringVar(&c.LocaleString, "l", defaultLocale, "locale")
+	c.flagSet.StringVar(&c.Voice, "v", defaultVoice, "voice")
+
+	if err = c.flagSet.Parse(os.Args[1:]); err != nil {
+		return err
+	}
+
+	if *help {
+		c.flagSet.Usage()
+		os.Exit(1)
+	}
 
 	return nil
+}
+
+func (c *App) printUsage() {
+	fmt.Println("\nUsage: tts [OPTIONS]\nGenerate batch audio files with speech synthesis service(Provided by Azure Cognitive Services)\n\nOptions:")
+	c.flagSet.PrintDefaults()
 }
